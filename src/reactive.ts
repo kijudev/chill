80a42e9912951d15a767fba475e593fc247e4e51
kijudev <dev@kiju.page>
2025-02-20 19:33:13 +0100

    Implement some funky reactive object stuff

--- src/reactive.ts
+++ src/reactive.ts
@@ -4,6 +4,18 @@ const __global__storage = new WeakMap<any, Set<WatchHandler>>();
 let __global__schedulerCurrentHandler: WatchHandler | null = null;
 let __global__schedulerSyncStorage: Set<WatchHandler> | null = null;
 
+function isObject(value: unknown): boolean {
+    return (
+        typeof value === "object" &&
+        value !== null &&
+        !Array.isArray(value) &&
+        !(value instanceof RegExp) &&
+        !(value instanceof Date) &&
+        !(value instanceof Set) &&
+        !(value instanceof Map)
+    );
+}
+
 function reactiveValue<T>(value: T): { value: T } {
     const obj = new Proxy(
         { value },
@@ -52,6 +64,72 @@ function reactiveValue<T>(value: T): { value: T } {
     return obj;
 }
 
+function reactiveObjectRaw<T extends object>(value: T, ctx?: Set<object>): T {
+    const proxy = new Proxy(value, {
+        get(target, key) {
+            if (!(key in target)) {
+                return undefined;
+            }
+
+            if (__global__schedulerCurrentHandler) {
+                __global__storage
+                    .get(proxy)
+                    ?.add(__global__schedulerCurrentHandler);
+            }
+
+            return target[key];
+        },
+        set(target, key, newValue) {
+            if (!(key in target)) {
+                return false;
+            }
+
+            target[key] = newValue;
+
+            let handlers = new Set<WatchHandler>();
+            __global__storage.get(proxy)?.forEach((handler) => {
+                handlers.add(handler);
+            });
+
+            if (ctx) {
+                ctx.forEach((obj) => {
+                    __global__storage.get(obj)?.forEach((handler) => {
+                        handlers.add(handler);
+                    });
+                });
+            }
+
+            if (__global__schedulerSyncStorage) {
+                handlers.forEach((handler) =>
+                    __global__schedulerSyncStorage?.add(handler),
+                );
+
+                return true;
+            }
+
+            handlers.forEach((handler) => {
+                handler();
+            });
+
+            return true;
+        },
+    });
+
+    __global__storage.set(proxy, new Set());
+
+    for (const [k, v] of Object.entries(value)) {
+        if (isObject(v)) {
+            proxy[k] = reactiveObjectRaw(v, new Set([proxy, ...(ctx ?? [])]));
+        }
+    }
+
+    return proxy;
+}
+
+function reactiveObject<T extends object>(value: T): { value: T } {
+    return reactiveObjectRaw({ value });
+}
+
 function sync(update: () => void) {
     __global__schedulerSyncStorage = new Set();
 
@@ -79,4 +157,10 @@ watch(() => {
 sync(() => {
     counta.value += 1;
     countb.value += 1;
+});
+
+const obj = reactiveObject({ a: { b: { c: 2 } } });
+
+watch(() => {
+    console.log({ obj: obj.value.a });
 });
