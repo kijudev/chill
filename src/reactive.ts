a4de195d169fd67feaadf01f557ccccac30f1ef4
kijudev <dev@kiju.page>
2025-02-21 13:54:05 +0100

    Rewrite the whole thing

--- src/reactive.ts
+++ src/reactive.ts
@@ -1,8 +1,17 @@
-type WatchHandler = () => void;
+type Effect = () => void;
 
-const __global__storage = new WeakMap<any, Set<WatchHandler>>();
-let __global__schedulerCurrentHandler: WatchHandler | null = null;
-let __global__schedulerSyncStorage: Set<WatchHandler> | null = null;
+type ReactiveStorage = {
+    dependencies: Set<Effect>;
+};
+
+type SyncStorage = {
+    effect: Effect;
+    dependecies: Set<Effect>;
+};
+
+const __global__reactiveStorage = new WeakMap<any, ReactiveStorage>();
+let __global__schedulerRunningEffect: Effect | null = null;
+let __global__schedulerSyncStorage: SyncStorage | null = null;
 
 function isObject(value: unknown): boolean {
     return (
@@ -16,151 +25,112 @@ function isObject(value: unknown): boolean {
     );
 }
 
+function trackDependencies(target: object) {
+    if (!__global__schedulerRunningEffect) {
+        return;
+    }
+
+    const storage = __global__reactiveStorage.get(target);
+    if (!storage) {
+        throw new Error("REACTIVE -> Reactive object does not exist.");
+    }
+
+    storage.dependencies.add(__global__schedulerRunningEffect);
+}
+
+function triggerDependencies(target: object) {
+    const storage = __global__reactiveStorage.get(target);
+    if (!storage) {
+        throw new Error("REACTIVE -> Reactive object does not exist.");
+    }
+
+    if (!__global__schedulerSyncStorage) {
+        storage.dependencies.forEach((dep) => dep());
+    } else {
+        storage.dependencies.forEach((dep) =>
+            __global__schedulerSyncStorage?.dependecies.add(dep),
+        );
+    }
+}
+
+function registerReactiveObject(target: object) {
+    if (__global__reactiveStorage.has(target)) {
+        throw new Error(
+            "REACTIVE -> A reactive object cannot be registered again",
+        );
+    }
+
+    __global__reactiveStorage.set(target, { dependencies: new Set() });
+}
+
 function reactiveValue<T>(value: T): { value: T } {
-    const obj = new Proxy(
+    const proxy = new Proxy(
         { value },
         {
-            get(target, key) {
+            get(target, key, receiver) {
                 if (key !== "value") {
-                    return undefined;
+                    throw new Error(
+                        "REACTIVE -> A reactive object has to be accessed through the 'value' property.",
+                    );
                 }
 
-                if (__global__schedulerCurrentHandler) {
-                    __global__storage
-                        .get(obj)
-                        ?.add(__global__schedulerCurrentHandler);
-                }
+                trackDependencies(proxy);
 
                 return target.value;
             },
-            set(target, key, newValue) {
+            set(target, key, newValue, reveiver) {
                 if (key !== "value") {
-                    return false;
+                    throw new Error(
+                        "REACTIVE -> A reactive object has to be accessed through the 'value' property.",
+                    );
                 }
 
                 target.value = newValue;
-
-                if (__global__schedulerSyncStorage) {
-                    __global__storage
-                        .get(obj)
-                        ?.forEach((handler) =>
-                            __global__schedulerSyncStorage?.add(handler),
-                        );
-
-                    return true;
-                }
-
-                __global__storage.get(obj)?.forEach((handler) => {
-                    handler();
-                });
+                triggerDependencies(proxy);
 
                 return true;
             },
         },
     );
 
-    __global__storage.set(obj, new Set());
-
-    return obj;
-}
-
-function reactiveObjectRaw<T extends object>(value: T, ctx?: Set<object>): T {
-    const proxy = new Proxy(value, {
-        get(target, key) {
-            if (!(key in target)) {
-                return undefined;
-            }
-
-            if (__global__schedulerCurrentHandler) {
-                __global__storage
-                    .get(proxy)
-                    ?.add(__global__schedulerCurrentHandler);
-            }
-
-            return target[key];
-        },
-        set(target, key, newValue) {
-            if (!(key in target)) {
-                return false;
-            }
-
-            target[key] = newValue;
-
-            let handlers = new Set<WatchHandler>();
-            __global__storage.get(proxy)?.forEach((handler) => {
-                handlers.add(handler);
-            });
-
-            if (ctx) {
-                ctx.forEach((obj) => {
-                    __global__storage.get(obj)?.forEach((handler) => {
-                        handlers.add(handler);
-                    });
-                });
-            }
-
-            if (__global__schedulerSyncStorage) {
-                handlers.forEach((handler) =>
-                    __global__schedulerSyncStorage?.add(handler),
-                );
-
-                return true;
-            }
-
-            handlers.forEach((handler) => {
-                handler();
-            });
-
-            return true;
-        },
-    });
-
-    __global__storage.set(proxy, new Set());
-
-    for (const [k, v] of Object.entries(value)) {
-        if (isObject(v)) {
-            proxy[k] = reactiveObjectRaw(v, new Set([proxy, ...(ctx ?? [])]));
-        }
-    }
-
+    registerReactiveObject(proxy);
     return proxy;
 }
 
-function reactiveObject<T extends object>(value: T): { value: T } {
-    return reactiveObjectRaw({ value });
-}
-
-function sync(update: () => void) {
-    __global__schedulerSyncStorage = new Set();
-
-    update();
-    for (const handler of __global__schedulerSyncStorage) {
-        handler();
+function sync(effect: Effect) {
+    if (__global__schedulerSyncStorage) {
+        throw new Error("REACTIVE - Something went horribly wrong");
     }
+
+    __global__schedulerSyncStorage = {
+        effect: effect,
+        dependecies: new Set(),
+    };
+
+    effect();
+    __global__schedulerSyncStorage?.dependecies.forEach((dep) => dep());
 
     __global__schedulerSyncStorage = null;
 }
 
-function watch(handler: WatchHandler) {
-    __global__schedulerCurrentHandler = handler;
-    sync(handler);
-    __global__schedulerCurrentHandler = null;
+function watch(effect: Effect) {
+    if (__global__schedulerRunningEffect) {
+        throw new Error("REACTIVE - Something went horribly wrong");
+    }
+
+    __global__schedulerRunningEffect = effect;
+    sync(effect);
+    __global__schedulerRunningEffect = null;
 }
 
-const counta = reactiveValue(0);
-const countb = reactiveValue(0);
+const count = reactiveValue(1);
+const count2 = reactiveValue(2);
 
 watch(() => {
-    console.log({ a: counta.value, b: countb.value });
+    console.log({ count: count.value, count2: count2.value });
 });
 
 sync(() => {
-    counta.value += 1;
-    countb.value += 1;
-});
-
-const obj = reactiveObject({ a: { b: { c: 2 } } });
-
-watch(() => {
-    console.log({ obj: obj.value.a });
+    count.value += 1;
+    count2.value += 1;
 });
